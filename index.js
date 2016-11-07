a47eba9428dc90fa180a0187d1a13c32cb2c8779
Mukesh Kumar <mukesh.kumar1@myntra.com>
2016-11-07 16:45:47 +0530

    load works

--- index.js
+++ index.js
@@ -7,56 +7,64 @@ const home = process.env.HOME;
 const cwd = process.cwd();
 const file = require('fs');
 var exec = require('child_process').execSync;
-const aliasRegex = /(^|\n)\s*alias\s+(.*)=\s*\'(.+)\'/g;
+const aliasRegex = /(^|\n)\s*alias\s+([^\s]*)\s*=\s*\'(.+)\'\s*(\n|;|&&)?/g;
+const pathRegex = /(^|\n)\s*export\s+PATH\s*=\s*(.+)\s*(\n|;|&&)?/g;
 
 const task = process.argv[2];
 
 switch(task){
   case 'init':
-    console.log('Setting up in path :', cwd);
+    console.log(yellow('Setting up in '+cwd));
     init();
     break;
   case 'load':
-    console.log('loading ...');
+    console.log(yellow('loading profile from ' + cwd));
     load();
     break;
   default:
-   console.log('Say what?! command:', task);
+   console.log('Unknown command :', task || 'did you mean `\x1B[31m al init \x1B[31m`?');
 }
 
 
 function init() {
   json.writeFileSync(join(cwd,'path.json'), process.env.PATH.split(':'), {spaces: 2});
-  let contents = file.readFileSync(join(home,'.bash_profile'));
-  let matches = [];
-  var match;
-  while ( ( match = aliasRegex.exec(contents) ) != null )
-  {
-      let x = {};
-      x[match[2]] = match[3];
-      matches.push(x);
-  }
+  let contents = file.existsSync(join(home,'.bash_profile')) ? file.readFileSync(join(home,'.bash_profile')) : '';
+  let matches = getAliases();
   json.writeFileSync(join(cwd,'alias.json'), matches, {spaces: 2});
 }
 
 function load(){
-  try{
-    console.log('Trying to read paths');
-    const paths = json.readFileSync(join(cwd,'path.json')).join(':');
-    const alias = json.readFileSync(join(cwd,'alias.json'));
-    const aliasScript = alias.map()
-    console.log('found paths');
-    console.log('Response:');
-    file.writeFileSync(join(cwd,'.profile'), '#!/bin/bash \nexport PATH=' + paths + ';\n');
-    console.log(exec('#!/bin/bash source ' + join(cwd,'path.sh')).toString('utf8'));
-  }
-  catch(e){
-    console.log('path/alias file not found', e);
-  }
-  try{
+    const paths = file.existsSync(join(cwd,'path.json')) ? json.readFileSync(join(cwd,'path.json')).join(':') : {};
+    const alias = file.existsSync(join(cwd,'alias.json')) ? json.readFileSync(join(cwd,'alias.json')) : {};
+    let source = paths.length ? 'export PATH=' + paths + ';\n' : '';
+    Object.keys(alias).forEach(function(key) { source += 'alias ' + key + '=\''+ alias[key]+'\';\n'; });
+    file.writeFileSync(join(cwd,'.profile'), '#!/usr/bin/env bash\n' + source);
+    console.log(red('Writing to ' + join(home, '.bash_profile')));
+    let contents = file.existsSync(join(home,'.bash_profile')) ? file.readFileSync(join(home,'.bash_profile'),'utf-8') : '';
+    contents = contents.replace(aliasRegex, '');
+    contents = contents.replace(pathRegex, '');
+    file.writeFileSync(join(home,'.bash_profile'), contents + '\n' + source);
+    console.log(green(join(home, '.bash_profile') + ' updated'));
+    console.log('To update current console run `'+green('source .profile') + '`');
+}
 
+function green(s){
+  return '\x1B[32m' + s + '\x1B[0m';
+}
+function red(s){
+  return '\x1B[31m' + s + '\x1B[0m';
+}
+
+function yellow(s){
+  return '\x1B[33m' + s + '\x1B[0m';
+}
+
+function getAliases(contents){
+  let matches = {};
+  var match;
+  while ( ( match = aliasRegex.exec(contents) ) != null )
+  {
+      matches[match[2]] = match[3];
   }
-  catch(e){
-    
-  }
+  return matches;
 }
