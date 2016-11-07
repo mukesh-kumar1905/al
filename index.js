996156c7406efe5072cfdc0898e40e04b54cc1f8
Mukesh Kumar <mukesh.kumar1@myntra.com>
2016-11-07 10:27:35 +0530

    load init

--- index.js
+++ index.js
@@ -5,27 +5,28 @@ const json = require('jsonfile');
 const join = require('path').join;
 const home = process.env.HOME;
 const cwd = process.cwd();
-const fs = require('fs');
 const file = require('fs');
-const paths = process.env.PATH.split(':');
+var exec = require('child_process').execSync;
 const aliasRegex = /(^|\n)\s*alias\s+(.*)=\s*\'(.+)\'/g;
 
 const task = process.argv[2];
 
 switch(task){
   case 'init':
-    console.log('Setting up');
+    console.log('Setting up in path :', cwd);
     init();
     break;
   case 'load':
     console.log('loading ...');
+    load();
     break;
   default:
-   console.log('Say what?! command: ' + task);
+   console.log('Say what?! command:', task);
 }
 
+
 function init() {
-  json.writeFileSync(join(cwd,'path.json'), paths, {spaces: 2});
+  json.writeFileSync(join(cwd,'path.json'), process.env.PATH.split(':'), {spaces: 2});
   let contents = file.readFileSync(join(home,'.bash_profile'));
   let matches = [];
   var match;
@@ -36,4 +37,26 @@ function init() {
       matches.push(x);
   }
   json.writeFileSync(join(cwd,'alias.json'), matches, {spaces: 2});
+}
+
+function load(){
+  try{
+    console.log('Trying to read paths');
+    const paths = json.readFileSync(join(cwd,'path.json')).join(':');
+    const alias = json.readFileSync(join(cwd,'alias.json'));
+    const aliasScript = alias.map()
+    console.log('found paths');
+    console.log('Response:');
+    file.writeFileSync(join(cwd,'.profile'), '#!/bin/bash \nexport PATH=' + paths + ';\n');
+    console.log(exec('#!/bin/bash source ' + join(cwd,'path.sh')).toString('utf8'));
+  }
+  catch(e){
+    console.log('path/alias file not found', e);
+  }
+  try{
+
+  }
+  catch(e){
+    
+  }
 }
