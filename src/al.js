c16f3bc0d49e910935985c2d9313cfa690c43f4a
Mukesh Kumar <mukesh.kumar1@myntra.com>
2016-11-08 13:27:56 +0530

    added checks for files

--- src/al.js
+++ src/al.js
@@ -5,7 +5,19 @@ var _constants = require('./constants');
 
 var _color = require('./color');
 
+var _fs = require('fs');
+
+var _fs2 = _interopRequireDefault(_fs);
+
+var _path = require('path');
+
 var _tasks = require('./tasks');
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var existsInCWD = function existsInCWD(name) {
+  return _fs2.default.existsSync((0, _path.join)(_constants.cwd, name));
+};
 
 var task = process.argv[2];
 
@@ -24,6 +36,10 @@ switch (task) {
     }
   case 'add':
     {
+      if (!existsInCWD('alias.json')) {
+        console.log((0, _color.red)('alias file does not exist. Are you in the correct folder ? Did you run `al init`?'));
+        process.exit();
+      }
       var alias = process.argv[3];
       var to = process.argv[4];
       if (!alias || !to) {
@@ -36,6 +52,10 @@ switch (task) {
     }
   case 'addp':
     {
+      if (!existsInCWD('path.json')) {
+        console.log((0, _color.red)('path file does not exist. Are you in the correct folder ? Did you run `al init`?'));
+        process.exit();
+      }
       var path = process.argv[3];
       if (!path) {
         console.log((0, _color.red)('Missing arguments. To add a path, correct format is ' + (0, _color.green)('al addp $PATH')));
